b20789743f288a0498915616f48662a675e7b4ea
Suppiluliuma <jesse.nevalainen@myy.haaga-helia.fi>
2023-09-25 07:51:25 +0300

    valmis

--- App.js
+++ App.js
@@ -1,20 +1,64 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, TextInput, Button, Alert } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import { api_key } from "./keys";
 
 export default function App() {
+  const [text, setText] = useState("");
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  const handleShow = () => {
+    fetch(
+      `https://www.mapquestapi.com/geocoding/v1/address?key=${api_key}&location=${text}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.results?.[0]?.locations?.[0]?.latLng) {
+          setLocation({
+            latitude: data.results[0].locations[0].latLng.lat,
+            longitude: data.results[0].locations[0].latLng.lng,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          });
+        } else {
+          Alert.alert("Error", "Osoitetta ei löytynyt.");
+        }
+      })
+      .catch((error) => {
+        Alert.alert("Error", error.message);
+      });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      {location.latitude !== 0 && location.longitude !== 0 ? (
+        <MapView
+          style={{ flex: 1 }}
+          initialRegion={location}
+        >
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        </MapView>
+      ) : (
+        <MapView style={{ flex: 1 }} /> 
+      )}
+      <View>
+        <TextInput
+          onChangeText={(newText) => setText(newText)}
+          value={text}
+          placeholder="Syötä osoite"
+        />
+        <Button title="Show" onPress={handleShow} />
+      </View>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ keys.js
@@ -0,0 +1,1 @@
+export const api_key = "qw80528aZZM5iwDqpbjMqlg8AAOge8rg";
